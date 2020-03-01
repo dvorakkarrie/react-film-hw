f62cf0f845a42264a027d086f1edc575013f1a10
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-03-01 16:59:59 -0600

    completed step 7

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
 import FilmRow from './FilmRow'
+// import FilmDetails from "./FilmDetails";
 
 class FilmListing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        filter: 'all'
-    };
-  }
 
   render() {
-    console.log(this.props)
     let allFilms = this.props.TMDB.films.map((film, index) => {
         return (
-            <div key={film.id}>{film.title}</div>
-        )
+            <FilmRow 
+                key={film.id} 
+                title={film.title} 
+                poster_path={film.poster_path}
+                release_date={film.release_date} />
+       )
     })
-    
+    console.log(this.props.TMDB.films)
+    console.log(allFilms)
     return (
       <div className="film-list">
-        <h1 className="section-title">FILMS</h1>
-        <h1 key="allFilms.film.id"><FilmRow allFilms={allFilms} /></h1>
+            {allFilms} 
       </div>
     );
   }
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
 
 class FilmRow extends Component {
-    render() {
+    render() {;
+        let year = new Date(this.props.release_date).getFullYear();
+        console.log(year)
+        let films=this.props
+        console.log(this.props.title)
+
         return (
-            <div>
-                {this.props.allFilms}
+            <div className="film-row">
+                <img src={`https://image.tmdb.org/t/p/w780${films.poster_path}`} alt={films.title} />  
+                <div className="film-summary">
+                    <h1>{films.title}</h1>
+                    <p>{year}</p>
+                </div>
             </div>
         )
     }
